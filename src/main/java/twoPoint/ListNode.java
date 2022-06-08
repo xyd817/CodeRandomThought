5e4d34245efcd6154a21e3325bed2b637f8d39ef
xyd <xyd@123.com>
2022-06-08 09:17:35 +0800

    2022/6/8 xyd commit ListNode

---
+++ src/main/java/twoPoint/ListNode.java
@@ -0,0 +1,19 @@
+package twoPoint;
+
+/**
+ * @author xyd
+ * @create 2022-06-08 9:15
+ */
+public class ListNode {
+    private int val;
+    private ListNode next;
+    public ListNode(){}
+    public ListNode(int val){
+        this.val = val;
+    }
+    public ListNode(int val,ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+
+}
