f6887ddaf3b97e53c682fcbef93d9663ff61cb92
xyd <xyd@123.com>
2022-06-08 09:29:06 +0800

    2022/6/8 xyd commit ReverseList

--- src/main/java/twoPoint/ReverseList.java
+++ src/main/java/twoPoint/ReverseList.java
@@ -1,5 +1,7 @@
 package twoPoint;
 
+import org.junit.Test;
+
 /**
  * @author xyd
  * @create 2022-06-08 9:18
@@ -13,8 +15,30 @@ public class ReverseList {
         ListNode temp = null;
         ListNode pre = null;
         while(cur != null){
-            temp = cur.
+            temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur= temp;
         }
-
+        return pre;
+    }
+    public ListNode createList(int[] nums){
+        ListNode after = null;
+        int n = nums.length;
+        for (int i = n - 1; i >= 0  ; i--) {
+            ListNode node = new ListNode(nums[i],after);
+            after = node;
+        }
+        return after;
+    }
+    @Test
+    public void test(){
+        int[] nums = {1,2,3,4,5};
+        ListNode head = createList(nums);
+        ListNode cur = reverseList(head);
+        while (cur != null){
+            System.out.print(cur.val + " ");
+            cur = cur.next;
+        }
     }
 }
