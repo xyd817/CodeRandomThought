7fa33027cefb58a080a1168e95bc5fec5b217f77
xyd <xyd@123.com>
2022-06-12 09:40:55 +0800

    2022/6/12 xyd commit ValidBracket

---
+++ src/main/java/stackandqueue/ValidBracket.java
@@ -0,0 +1,52 @@
+package stackandqueue;
+
+import org.junit.Test;
+import scala.Char;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * leetcode 20
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
+ * 有效字符串需满足：
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author xyd
+ * @create 2022-06-12 9:11
+ */
+public class ValidBracket {
+    public boolean isValid(String s) {
+        if ("".equals(s)) {
+            return false;
+        }
+        Deque<Character> stack = new LinkedList<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (stack.isEmpty() || stack.peek() != s.charAt(i)) {
+                return false;
+            } else {
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    @Test
+    public void test() {
+        String s = "[{}]))";
+        System.out.println(isValid(s));
+    }
+}
