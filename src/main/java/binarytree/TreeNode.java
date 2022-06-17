b59b7a983d4a2e3533495d41bdfdb9f6e448b17b
xyd <xyd@123.com>
2022-06-17 09:32:41 +0800

    2022/6/17 xyd commit TreeNode

---
+++ src/main/java/binarytree/TreeNode.java
@@ -0,0 +1,19 @@
+package binarytree;
+
+/**
+ * @author xyd
+ * @create 2022-06-17 9:29
+ */
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
