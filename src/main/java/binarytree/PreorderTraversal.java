86720a75a397358ba2361018ca15965bd912fde7
xyd <xyd@123.com>
2022-06-18 08:34:26 +0800

    2022/6/18 xyd commit InorderTraversal

--- src/main/java/binarytree/PreorderTraversal.java
+++ src/main/java/binarytree/PreorderTraversal.java
@@ -1,11 +1,10 @@
 package binarytree;
 
 import org.junit.Test;
+import scala.reflect.internal.Trees;
 
 import javax.print.DocFlavor;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * leetcode 144
@@ -29,12 +28,31 @@ public class PreorderTraversal {
         preOrder(root.left, res);
         preOrder(root.right, res);
     }
-
+    //使用迭代法完成前序遍历
+    public List<Integer> preorderTraversal1(TreeNode root) {
+        if(root == null){
+            return new ArrayList<>();
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        List<Integer> ans = new ArrayList<>();
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            ans.add(node.val);
+            if(node.right != null){
+                stack.push(node.right);
+            }
+            if(node.left != null){
+                stack.push(node.left);
+            }
+        }
+        return ans;
+    }
     @Test
     public void test(){
         String[] roots = {"1","null","2","3"};
         TreeNode root = CreateTreeNode.createBinaryTree(roots);
-        List<Integer> list = preorderTraversal(root);
+        List<Integer> list = preorderTraversal1(root);
         list.forEach(System.out::println);
 
     }
